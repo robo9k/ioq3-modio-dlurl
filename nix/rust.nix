59a0184b7bae6383d0e1ab495f7e923f2aba02d4
robot9001 <robo9k@symlink.io>
2026-02-23 13:41:32 +0100

    Remove unused Nix config
    Signed-off-by: robot9001 <robo9k@symlink.io>

--- nix/rust.nix
+++
@@ -1,7 +0,0 @@
-{ sources ? import ./sources.nix }:
-
-let
-  pkgs =
-    import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
-  chan = (pkgs.rustChannelOf { rustToolchain = ../rust-toolchain; }).rust;
-in chan
